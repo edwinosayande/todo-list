485bff01ef808ec482a26d91f00466e1dc74cf96
Osayande Edwin Eboseta <94188588+edwinosayande@users.noreply.github.com>
2022-06-30 07:38:53 -0700

    Add initial website todo-list

---
+++ app.js
@@ -0,0 +1,105 @@
+const alert = document.querySelector('.alert');
+const form = document.querySelector('.grocery-form');
+const grocery = document.getElementById('grocery');
+const submitBtn = document.querySelector('.submit-btn');
+const container = document.querySelector('.grocery-container');
+const list = document.querySelector('.grocery-list');
+const clearBtn = document.querySelector('.clear-btn');
+
+let editElement;
+let editFlag = false;
+let editID = "";
+
+form.addEventListener('submit', addItem);
+clearBtn.addEventListener('click', clearItems);
+
+function addItem(e) {
+    e.preventDefault();
+    const value = grocery.value;
+    grocery.value = "";
+    const id = new Date().getTime().toString();
+    if(value && !editFlag) {
+        const element = document.createElement('article');
+        element.classList.add('grocery-item');
+        const attribute = document.createAttribute('data-id');
+        attribute.value =id;
+        element.setAttributeNode(attribute);
+        element.innerHTML = `<p class="title">${value}</p>
+        <div class="btn-cotainer">
+            <button type="button" class="edit-btn">
+                <i class="fas fa-edit">EDIT</i>
+                
+            </button>
+            <button type="button" class="delete-btn">
+                    <i class="fas fa-trash"><img src="cancel.png" alt=""></i>
+                    
+                </button>
+        </div>`;
+
+        const deleteBtn = element.querySelector('.delete-btn');
+        const editBtn = element.querySelector('.edit-btn');
+        deleteBtn.addEventListener('click', deleteItem);
+        editBtn.addEventListener('click', editItem);
+        list.appendChild(element);
+        displayAlert('successfully added item', 'success');
+        container.classList.add('show-container');
+        addToLocalStorage(id, value);
+        setBackToDefault();
+    }else if(value && editFlag){
+        editElement.innerHTML = value;
+        displayAlert('value changed', 'success');
+        editLocalStorage(editID, value);
+        setBackToDefault()
+
+
+    }else{
+        displayAlert('the value is empty', 'danger');
+    }
+}
+
+function displayAlert(text, action) {
+    alert.textContent = text;
+    alert.classList.add(`alert-${action}`);
+
+    setTimeout(function(){
+        alert.textContent = "";
+        alert.classList.remove(`alert-${action}`);
+    },2000)
+} 
+function clearItems() {
+    const items = document.querySelectorAll('.grocery-item');
+    if(items.length > 0){
+items.forEach(function(item){
+    list.removeChild(item);
+});
+    }
+    container.classList.remove('show-container');
+    displayAlert('Cleared All Items', 'success');
+    setBackToDefault()
+}
+
+function deleteItem(e) {
+    const element = e.currentTarget.parentElement.parentElement;
+    const id = element.dataset.id;
+    list.removeChild(element);
+    if(list.children.length === 0) {
+        container.classList.remove('show-container');
+    }
+    displayAlert('item removed', 'danger');
+    setBackToDefault();
+
+}
+function editItem(e) {
+    const element = e.currentTarget.parentElement.parentElement;
+    editElement = e.currentTarget.parentElement.previousElementSibling;
+    grocery.value = editElement.innerHTML;
+    editFlag = true;
+    editID = element.dataset.id;
+    submitBtn.textContent = 'update';
+}
+function setBackToDefault() {
+    grocery.value = "";
+    editFlag = false;
+    editID = "";
+    submitBtn.textContent = 'submit';
+}
